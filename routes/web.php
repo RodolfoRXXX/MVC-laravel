7537034faebf9c9d477741d7abf59cea9f9cb3eb
Rodolfo <rodolfo.singular@gmail.com>
2022-04-10 09:47:51 -0300

    10042022 0947

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use GuzzleHttp\Psr7\Uri;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/inicio', function()
+{
+    return view('inicio');
 });
